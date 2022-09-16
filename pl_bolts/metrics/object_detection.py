bb2304afc0449b91908685ae4ac7628c5d5117e7
BaruchG <bgutow@gmail.com>
2022-09-16 15:28:33 -0400

    removed under review

--- pl_bolts/metrics/object_detection.py
+++ pl_bolts/metrics/object_detection.py
@@ -1,10 +1,7 @@
 import torch
 from torch import Tensor
 
-from pl_bolts.utils.stability import under_review
 
-
-@under_review()
 def iou(preds: Tensor, target: Tensor) -> Tensor:
     """Calculates the intersection over union.
 
@@ -37,7 +34,6 @@ def iou(preds: Tensor, target: Tensor) -> Tensor:
     return iou
 
 
-@under_review()
 def giou(preds: Tensor, target: Tensor) -> Tensor:
     """Calculates the generalized intersection over union.
 
